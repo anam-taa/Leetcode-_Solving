351c78c22538e3b7b3d51e99e62e97ab6458c103
anamta ansari <ansari.anamta9721@gmail.com>
2026-09-08 00:00:41 +0530

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.java
+++ LeetCode/Group-Anagrams/Solution.java
@@ -1,1 +1,17 @@
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
+1class Solution {
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String, List<String>> ans = new HashMap<>();
+4
+5        for (String s : strs) {
+6            char[] chars = s.toCharArray();
+7            Arrays.sort(chars);
+8            String key = new String(chars);
+9            if (!ans.containsKey(key)) {
+10                ans.put(key, new ArrayList<>());
+11            }
+12            ans.get(key).add(s);
+13        }
+14
+15        return new ArrayList<>(ans.values());        
+16    }
+17}
